615e4a675a4fbc42e42c126f7ea164eafacf9d61
fanlilian <lilianfan@live.ca>
2020-08-09 18:19:10 -0400

    Updates

---
+++ precache-manifest.2e5fd09bd18d39d1f53f426987d0e8c8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37305354bcb2204c13bcd434d1c57549",
+    "url": "/personal-website/index.html"
+  },
+  {
+    "revision": "dfbc74cf9424ebc857b1",
+    "url": "/personal-website/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7f7506d485d96d436dd5",
+    "url": "/personal-website/static/js/2.4cbe87e7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/personal-website/static/js/2.4cbe87e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfbc74cf9424ebc857b1",
+    "url": "/personal-website/static/js/main.20afe2d8.chunk.js"
+  },
+  {
+    "revision": "575b6f83863d43fff81c",
+    "url": "/personal-website/static/js/runtime-main.fd9e9e8b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/personal-website/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e87935bc08fa0a5036759cafbc07ed91.js"
+  "/personal-website/precache-manifest.2e5fd09bd18d39d1f53f426987d0e8c8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/personal-website/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
